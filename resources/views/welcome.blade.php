b9c503e796ecafe0e91ea70ed2e11c26c2b3abf3
hotwax2016 <kiru180815@protonmail.com>
2021-05-01 18:01:59 +0530

    added welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,19 @@
     <title>Welcome to OneSyntax</title>
 </head>
 <body>
-    <main class="bg-gray-300">
+    <main class="flex justify-center items-center bg-gray-300 h-screen">
+        <div class="flex items-center w-1/2 h-2/3">
+            <div class="-mt-24">
+                <div>
+                    <h1 class="text-center text-6xl text-gray-600">Welcome to OneSyntax</h1>
+                </div>
+                <div class="pt-4">
+                    <h3 class="text-center text-2xl text-gray-600">
+                        Please <span class="text-gray-500 hover:underline cursor-pointer"><a href="/login">login</a></span> to continue
+                    </h3>
+                </div>
+            </div>
+        </div>
 
     </main>
 </body>
