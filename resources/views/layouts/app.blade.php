5a70f3a4dad6f67a07107bcdc1e9961fa2219aa8
hotwax2016 <kiru180815@protonmail.com>
2021-05-02 13:10:08 +0530

    changed the main theme color from purple to blue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,30 +22,30 @@
 
 </head>
 <body>
-    <div class="flex bg-red-300 h-screen">
-        <div class="w-2/12 bg-purple-500 text-purple-200">
+    <div class="flex bg-red-300 min-h-screen">
+        <div class="w-2/12 bg-blue-500 text-blue-200">
             <div class="flex justify-center items-center h-16">
                 OneSyntax
             </div>
             <div class="pt-5 px-4">
                 <ul>
-                    <li class="px-2 py-3 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer"><a href="/dashboard">Dashboard</a></li>
-                    <li class="px-2 py-3 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer">Employee Management</li>
-                    <li class="px-2 py-3 text-md font-semibold hover:text-purple-100 rounded-md cursor-pointer">System Management</li>
+                    <li class="px-2 py-3 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer"><a href="/dashboard">Dashboard</a></li>
+                    <li class="px-2 py-3 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer">Employee Management</li>
+                    <li class="px-2 py-3 text-md font-semibold hover:text-blue-100 rounded-md cursor-pointer">System Management</li>
                     <li>
                         <ul>
-                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer">Country</li>
-                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer">State</li>
-                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer">City</li>
-                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer">Department</li>
+                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer">Country</li>
+                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer">State</li>
+                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer">City</li>
+                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer">Department</li>
                         </ul>
                     </li>
-                    <li class="px-2 py-3 text-md font-semibold hover:text-purple-100 rounded-md cursor-pointer">User Management</li>
+                    <li class="px-2 py-3 text-md font-semibold hover:text-blue-100 rounded-md cursor-pointer">User Management</li>
                     <li>
                         <ul>
-                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer"><a href="/users">User</a></li>
-                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer">Role</li>
-                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-purple-200 hover:text-purple-500 rounded-md cursor-pointer">Permission</li>
+                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer"><a href="/users">User</a></li>
+                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer">Role</li>
+                            <li class="pl-8 px-2 py-2 text-md font-semibold hover:bg-blue-200 hover:text-blue-500 rounded-md cursor-pointer">Permission</li>
                         </ul>
                     </li>
                 </ul>
@@ -53,13 +53,13 @@
         </div>
         <div class="w-10/12 bg-gray-100">
             <div class="h-16 bg-white shadow">
-                <div class="flex justify-end items-center h-full px-8 text-purple-500">
+                <div class="flex justify-end items-center h-full px-8 text-blue-500">
                     <div>
                         @auth
                             {{ Auth::user()->name }}
                         @endauth
                     </div>
-                    <div class="ml-3 cursor-pointer px-3 py-2 bg-purple-100 rounded-md hover:bg-purple-200">
+                    <div class="ml-3 cursor-pointer px-3 py-2 bg-blue-100 rounded-md hover:bg-blue-200">
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             Logout
                         </a>
@@ -70,7 +70,7 @@
                     </div>
                 </div>
             </div>
-            <div class="mt-1 p-4 bg-gray-100">
+            <div class="mt-1 p-4">
                 <div>
                     @yield('main')
                 </div>
